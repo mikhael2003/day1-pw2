001bff4e18bf3bc679bce49c90191d5027661d65
mikhael2003 <mikhaelsiman2003@gmail.com>
2023-02-21 14:36:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
     // ini adalah perintah untuk mencetak pesan ke layar 
     echo "Hello world";
 
+    /* 
+    ini adalah contoh deklarasi variable dengan tipe data.
+    teks, bilngan bulat, bilangan desimanl, desimal
+    */
 
 
+    $npm = "2125250075";
+    $nama = "Mikhael Anrodin Simanjuntak";
+    $jk = "Laki-laki";
+    $usia = "18";
+    $tinggi = "168,5";
+
+    echo "<br/>";
+    echo "NPM :" ; 
+    echo $npm;
 ?>
